6b8d45971898623cb23292c60479014e158d0998
Andrew Holguin <ajholguin1@gmail.com>
2015-03-30 22:07:17 -0700

    enhanced heatmap 8760

--- R/plotHeatmap_8760.R
+++ R/plotHeatmap_8760.R
@@ -5,14 +5,23 @@
 #' @return \code{NULL}
 #' @export
 
-plotHeatmap_8760 <- function (dat) {
+plotHeatmap_8760 <- function (dat, var, normVar, inclWeekends, zRng) {
   
+  if(inclWeekends == FALSE) dat <- filter(dat, dow %in% 2:6)
+  
+  plot_dat <- dat %>%
+    group_by(doy,hour) %>%
+    summarise_(var = lazyeval::interp(~mean(var, na.rm = TRUE), var = as.name(var)))
+  
+  if (normVar != 'none') plot_dat <- plot_dat %>% group_by_(normVar) %>% mutate(var = var / max(var, na.rm=TRUE))
+    
   p <- ggplot(dat, aes(hour, doy)) +
-    geom_tile(aes(fill = kW), colour = "white") +
-    scale_fill_gradientn(colours = c("#FFE97E", "#FF7800", "#700000")) +
+    geom_tile(aes_string(fill = var), colour = "white") +
+    scale_fill_gradientn(colours = c("#FFE97E", "#FF7800", "#700000"), limits=zRng) +
     scale_x_discrete(expand = c(0,0), limits=0:23, breaks=seq(0,23,by=2)) +
     scale_y_continuous(expand = c(0,0), limits=c(366,1), trans='reverse', breaks=seq(10,360,by=10)) +
-    ylab('day of year')
+    ylab('day of year') +
+    theme(legend.position = "top", legend.direction="horizontal")
   
   print(p)
   
